05795ec458d793d0958fa9f8e807f6151ec9ee5c
Yusuf Berki YAZICIOĞLU <mail@yusufberki.net>
2022-08-21 13:35:00 +0300

    Add last youtube videos

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -30,5 +30,44 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+    loadVideos();
 
 });
+
+var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=UCYhQVqYj05Cqa44QrE48HQA");
+var numberOfVideo = 10
+
+function loadVideos() {
+    function putVideos(data) {
+        console.log("data", data)
+        var htmlContent = ""
+
+        numberOfVideo = numberOfVideo >= data.items.length ? data.items.length : numberOfVideo
+        
+        for (let videoNumber = 0; videoNumber < numberOfVideo; videoNumber++) {
+            var link = data.items[videoNumber].link;
+            var title = data.items[videoNumber].title;
+
+            htmlContent += `<li>
+            <span class="fa-li"><i class="fab fa-youtube text-danger"></i></span>
+            <a href="${link}">${title}</a>
+        </li>`;
+        }
+        document.getElementById('yt-videos').innerHTML = htmlContent;
+
+    }
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3DUCYhQVqYj05Cqa44QrE48HQA", requestOptions)
+        .then(response => response.json())
+        .then(result => putVideos(result))
+        .catch(error => console.log('error', error));
+
+
+}
+
+
